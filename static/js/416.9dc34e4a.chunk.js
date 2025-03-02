1fa1e8d5d3be3252372ccb1f42ae1f014a8d2c3c
Martin Ochieng <martinochieng9360@gmail.com>
2025-03-02 15:17:44 +0300

    Updates

---
+++ static/js/416.9dc34e4a.chunk.js
@@ -0,0 +1,45 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[416],{9416:(e,t,n)=>{n.r(t),n.d(t,{default:()=>y});var i=n(1319),r=n(4647),d=n(9281),s=n(2080),l=n(2382),a=n(5639),c=n(2646),o=n(9);const x=(0,o.Ay)("section")`
+    position: relative;
+    padding: 7.5rem 0 3rem;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+
+    @media screen and (max-width: 1024px) {
+        padding: 5.5rem 0 3rem;
+    }
+`,p=(0,o.Ay)("p")`
+    padding: 0.75rem 0 0.75rem;
+`,h=(0,o.Ay)("div")`
+    max-width: 570px;
+
+    @media only screen and (max-width: 768px) {
+        max-width: 100%;
+    }
+`,m=o.Ay.div`
+    width: 100%;
+    max-width: 500px;  // Adjust width as needed
+    margin: 0 auto;
+    text-align: center;
+`,g=o.Ay.div`
+    display: flex;
+    flex-direction: column;  // Stack icon above text
+    align-items: center;     // Center horizontally
+    justify-content: center; // Center vertically
+    text-align: center;
+    gap: 10px;              // Add space between icon & text
+
+    img {
+        display: block;        // Ensure it's treated as a block element
+        margin: 0 auto;        // Center horizontally
+    }
+
+    p {
+        font-size: 16px;
+        color: #333;
+        margin: 0;
+        padding: 0;
+        text-align: center;
+    }
+`;var u=n(579);const y=(0,d.C)()((e=>{let{title:t,content:n,button:d,sliderContent:o,t:y}=e;return(0,u.jsx)(x,{children:(0,u.jsx)(s.q7,{direction:"up",triggerOnce:!0,children:(0,u.jsx)(i.A,{justify:"center",align:"middle",children:(0,u.jsx)(h,{children:(0,u.jsxs)(r.A,{lg:24,md:24,sm:24,xs:24,children:[(0,u.jsx)("h6",{children:y(t)}),(0,u.jsx)(p,{children:y(n)}),o&&o.length>0&&(0,u.jsx)(m,{children:(0,u.jsx)(l.A,{dots:!1,infinite:!0,speed:500,slidesToShow:1,slidesToScroll:1,autoplay:!0,autoplaySpeed:3e3,arrows:!1,children:o.map(((e,t)=>(0,u.jsxs)(g,{children:[(0,u.jsx)(c.A,{src:e.icon,width:"150px",height:"150px"}),(0,u.jsx)("br",{}),(0,u.jsx)("p",{children:y(e.text)})]},t)))})}),d&&(0,u.jsx)(a.$,{name:"submit",onClick:()=>{return e="mission",void document.getElementById(e).scrollIntoView({behavior:"smooth"});var e},children:y(d)})]})})})})})}))}}]);
+//# sourceMappingURL=416.9dc34e4a.chunk.js.map
---
+++ static/js/453.5f441d32.chunk.js
@@ -0,0 +1,37 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[453],{453:(e,i,t)=>{t.r(i),t.d(i,{default:()=>x});var d=t(1319),r=t(4647),n=t(9281),s=t(2080),l=t(9);const a=(0,l.Ay)("p")`
+  margin-top: 1.5rem;
+`,o=(0,l.Ay)("div")`
+  position: relative;
+  max-width: 700px;
+`,m=(0,l.Ay)("div")`
+  border-radius: 3rem;
+  max-width: 400px;
+`;var c=t(579);const h=(0,n.C)()((e=>{let{title:i,content:t,t:d}=e;return(0,c.jsxs)(o,{children:[(0,c.jsx)("h6",{children:d(i)}),(0,c.jsx)(m,{children:(0,c.jsx)(a,{children:d(t)})})]})})),p=(0,l.Ay)("div")`
+  padding: 5rem 0;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 3rem 0;
+  }
+`,x=((0,l.Ay)("form")`
+  width: 100%;
+  max-width: 520px;
+
+  @media only screen and (max-width: 1045px) {
+    max-width: 100%;
+    margin-top: 2rem;
+  }
+`,(0,l.Ay)("span")`
+  display: block;
+  font-weight: 600;
+  color: rgb(255, 130, 92);
+  height: 0.775rem;
+  padding: 0 0.675rem;
+`,(0,l.Ay)("div")`
+  text-align: end;
+  position: relative;
+
+  @media only screen and (max-width: 414px) {
+    padding-top: 0.75rem;
+  }
+`,(0,n.C)()((e=>{let{title:i,content:t,id:n}=e;return(0,c.jsx)(p,{id:n,children:(0,c.jsxs)(d.A,{justify:"space-between",align:"middle",children:[(0,c.jsx)(r.A,{lg:12,md:11,sm:24,xs:24,children:(0,c.jsx)(s.q7,{direction:"left",triggerOnce:!0,children:(0,c.jsx)(h,{title:i,content:t})})}),(0,c.jsx)(r.A,{lg:12,md:12,sm:24,xs:24,children:(0,c.jsx)(s.q7,{direction:"right",triggerOnce:!0,children:(0,c.jsx)("iframe",{src:"https://docs.google.com/forms/d/1Hf1v4MNbQd_HYhvuAcCGW4K2X3Bet2vwzTZ9hkZ7pLs/viewform",width:"100%",height:"600px",style:{border:"none"},title:"Google Form"})})})]})})})))}}]);
+//# sourceMappingURL=453.5f441d32.chunk.js.map
